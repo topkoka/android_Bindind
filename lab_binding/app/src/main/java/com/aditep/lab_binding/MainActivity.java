f2aa27983674b5f39bdbf2193d26c9a2984ff85d
aditep <topkoka4000@gmail.com>
2018-06-05 22:17:42 +0700

    lab-v2.0

--- lab_binding/app/src/main/java/com/aditep/lab_binding/MainActivity.java
+++ lab_binding/app/src/main/java/com/aditep/lab_binding/MainActivity.java
@@ -1,13 +1,17 @@
 package com.aditep.lab_binding;
 
-import android.support.v7.app.AppCompatActivity;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.aditep.lab_binding.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
     }
 }
