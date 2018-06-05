239eeed7cc2cadae03494ae519e754ed014a245c
aditep <topkoka4000@gmail.com>
2018-06-05 22:23:57 +0700

    lab-v2.1

--- lab_binding/app/src/main/java/com/aditep/lab_binding/MainActivity.java
+++ lab_binding/app/src/main/java/com/aditep/lab_binding/MainActivity.java
@@ -8,10 +8,14 @@ import com.aditep.lab_binding.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
     ActivityMainBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        setShowtext();
 
+    } private void setShowtext(){
+            binding.tvhello.setText("TEST_setTEXT Binding");
     }
 }
